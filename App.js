d8c394f567eeccf9508b654a465714e44d8f8a76
Patrick <patrick@Patricks-MacBook-Pro.local>
2019-07-29 18:48:39 +0200

    added react-navigation, react-native-gesture-handler and react-native-screens

--- App.js
+++ App.js
@@ -6,7 +6,7 @@
  * @flow
  */
 
-import React, {Fragment} from 'react';
+import React, { Fragment } from 'react';
 import {
   SafeAreaView,
   StyleSheet,
@@ -23,6 +23,10 @@ import {
   DebugInstructions,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
+
+import { useScreens } from 'react-native-screens';
+
+useScreens();
 
 const App = () => {
   return (
